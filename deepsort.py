aa1bf4b7a599d67c5c7595fa898150ce6fdac963
AvitalZar <avital.zar@msmail.ariel.ac.il>
2026-04-09 09:48:18 +0300

    explaining a little

--- deepsort.py
+++ deepsort.py
@@ -1,3 +1,4 @@
+# it's just that if I'd try string the input within the recursion it would sort everything in alephbeitic order. I think.
 def deep_sorted(x:any)->str:
     return str(deep_sorted_help(x))
 
